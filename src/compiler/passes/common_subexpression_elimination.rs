78e21ea136638d86c9de8937dffa0df1a10b630c
Batmend Batsaikhan <batmend.batsaikhan@reilabs.io>
2026-01-28 14:51:44 +0800

    feat: implemented cse for tuple proj

--- src/compiler/passes/common_subexpression_elimination.rs
+++ src/compiler/passes/common_subexpression_elimination.rs
@@ -5,7 +5,7 @@ use std::{
 
 use crate::compiler::{
     flow_analysis::{CFG, FlowAnalysis},
-    ssa::{BinaryArithOpKind, BlockId, CmpKind, Const, Function, OpCode, SSA, ValueId},
+    ssa::{BinaryArithOpKind, BlockId, CmpKind, Const, Function, OpCode, SSA, TupleIdx, ValueId},
 };
 use crate::compiler::{pass_manager::Pass, passes::fix_double_jumps::ValueReplacements};
 
@@ -23,6 +23,7 @@ enum Expr {
     And(Vec<Expr>),
     Select(Box<Expr>, Box<Expr>, Box<Expr>),
     ArrayGet(Box<Expr>, Box<Expr>),
+    TupleGet(Box<Expr>, Box<Expr>),
     Not(Box<Expr>),
     ReadGlobal(u64),
 }
@@ -99,7 +100,7 @@ impl Expr {
     }
 
     pub fn tuple_get(&self, index: &Self) -> Self {
-        Self::ArrayGet(Box::new(self.clone()), Box::new(index.clone()))
+        Self::TupleGet(Box::new(self.clone()), Box::new(index.clone()))
     }
 
     pub fn select(&self, then: &Self, otherwise: &Self) -> Self {
@@ -156,6 +157,7 @@ impl Display for Expr {
                 write!(f, "({} ? {} : {})", cond, then, otherwise)
             }
             Self::ArrayGet(array, index) => write!(f, "{}[{}]", array, index),
+            Self::TupleGet(tuple, index) => write!(f, "{}.{}", tuple, index),
             Self::Not(value) => write!(f, "(~{})", value),
             Self::ReadGlobal(index) => write!(f, "g{}", index),
         }
@@ -443,12 +445,23 @@ impl CSE {
                             *r,
                         ));
                     }
-                    OpCode::TupleProj { 
-                        result: _,
-                        tuple: _,
-                        idx: _,
+                    OpCode::TupleProj {
+                        result: r,
+                        tuple,
+                        idx,
                     } => {
-                        todo!("Implement TupleProj expression handling")
+                        let tuple_expr = get_expr(&exprs, tuple);
+                        let index_expr = match idx {
+                            TupleIdx::Static(i) => Expr::UConst(64, *i as u128),
+                            TupleIdx::Dynamic(idx_value, _) => get_expr(&exprs, idx_value),
+                        };
+                        let result_expr = tuple_expr.tuple_get(&index_expr);
+                        exprs.insert(*r, result_expr.clone());
+                        result.entry(result_expr).or_default().push((
+                            block_id,
+                            instruction_idx,
+                            *r,
+                        ));
                     },
                     OpCode::MkTuple { 
                         result: _,
