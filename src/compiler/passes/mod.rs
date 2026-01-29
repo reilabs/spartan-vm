4e6e96becf2c4393a96e257da1117d93c0914651
Marcin Kostrzewa <marckostrzewa@gmail.com>
2026-01-29 16:56:50 +0000

    Remove unreachable blocks to not confuse further passes

--- src/compiler/passes/mod.rs
+++ src/compiler/passes/mod.rs
@@ -9,6 +9,7 @@ pub mod fix_double_jumps;
 pub mod mem2reg;
 pub mod pull_into_assert;
 pub mod rc_insertion;
+pub mod remove_unreachable_blocks;
 pub mod specializer;
 pub mod witness_write_to_fresh;
 pub mod witness_write_to_void;
